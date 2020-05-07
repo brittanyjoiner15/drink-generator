56a455eb4d8dbd8b3ee63836ad2e3ade0aa90020
Brittany Joiner <brittanyjoiner15@gmail.com>
2020-05-06 19:47:17 -0500

    added header and form for selecting drinks

---
+++ src/components/ChooseDrink.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export class ChooseDrink extends React.Component {
+  render() {
+    return (
+      <div className="Container-fluid py-5">
+        <div classNam="Row">
+          <div className="Col">
+            <h3>Pick your poison</h3>
+            <form>
+              <div className="form-group">
+                <select className="form-control-lg" id="cars" name="cars">
+                  <option value="whiskey">Whiskey</option>
+                  <option value="rum">Rum</option>
+                  <option value="bourbon">Bourbon</option>
+                  <option value="vodka">Vodka</option>
+                  <option value="gin">Gin</option>
+                  <option value="kahlua">Kahlua</option>
+                </select>
+              </div>
+              <button type="submit" className="btn-primary btn-lg">
+                Show Me Drinks
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class Header extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid topper py-5">
+        <h1>Drink Recipe Generator</h1>
+      </div>
+    );
+  }
+}
